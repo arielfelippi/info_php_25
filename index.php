2e91bc82c34c851b8bfbda5b20dd10e1114a883f
suporte <suporte@BG-LABX-PCX>
2025-05-20 21:34:08 -0300

    exercicios IF ELSE

--- index.php
+++ index.php
@@ -1,20 +1,17 @@
 <?php
 
-echo "Ola Mundo! <br> ";
-
-// como fazer uma soma no PHP
-$x = 2 + 2;
+/**
+ * 1-) Verificar a partir de um ano(numero) informado pelo usuario
+ * Qual a sua idade e se o mesmo é maior de idade. Exibir a idade calculada 
+ * e a mensagem innformando sua maioridade ou não.
+ * 
+ * DICA: Utilizar variáveis, date("Y") e if else.
+ */
 
 /*
- echo $x;
+ 2-) Dada as notas de 4 provas e, a media para ser aprovado de ano sendo 7.
+    Calcular se o aluno atingiu a media e informar se o mesmo foi aprovado.
+    Dica: média aritmetica.
 
-echo "<br>";
-
-$x = 20 + 20 ;
-
-echo $x;
+    Tema de casa: calcular tambem as médias: ponderada e harmonica.
 */
-
-$x = 30 + 30 ;
-
-echo $x;
