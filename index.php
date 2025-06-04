2ae0cccff2148aac29c549d672573fa058447b77
suporte <suporte@BG-LABX-PCX>
2025-06-03 21:32:49 -0300

    exercicios de reforco lacos for e while

--- index.php
+++ index.php
@@ -35,100 +35,100 @@
 // }
 
 // Criar uma calculadora
-$valor1 = 10; 
-$valor2 = 10;
-$operador = "*";
-$resultado = 0;
+// $valor1 = 10; 
+// $valor2 = 10;
+// $operador = "*";
+// $resultado = 0;
 
-switch ($operador) {
-    case "+":
-        $resultado = $valor1 + $valor2;
-        break;
-    case "-":
-        $resultado = $valor1 - $valor2;
-        break;
-    case "*":
-        $resultado = $valor1 * $valor2;
-        break;
-    case "/":
-        $resultado = $valor1 / $valor2;
-        break;
-    default:
-        echo "Operador inválido.";
-        $resultado = 0;
-        break;
-}
+// switch ($operador) {
+//     case "+":
+//         $resultado = $valor1 + $valor2;
+//         break;
+//     case "-":
+//         $resultado = $valor1 - $valor2;
+//         break;
+//     case "*":
+//         $resultado = $valor1 * $valor2;
+//         break;
+//     case "/":
+//         $resultado = $valor1 / $valor2;
+//         break;
+//     default:
+//         echo "Operador inválido.";
+//         $resultado = 0;
+//         break;
+// }
 
-echo "O resultado da operação($valor1 $operador $valor2) é: $resultado";
+// echo "O resultado da operação($valor1 $operador $valor2) é: $resultado";
 
-echo "<br><br>";
+// echo "<br><br>";
 
-if ($operador == "+") {
-    $resultado = $valor1 + $valor2;
-}
-else if ($operador == "-") {
-    $resultado = $valor1 - $valor2;
-}
-else if ($operador == "*") {
-    $resultado = $valor1 * $valor2;
-}
-else if ($operador == "/") {
-    $resultado = $valor1 / $valor2;
-}
-else {
-    echo "Operador inválido.";
-    $resultado = 0;
-}
+// if ($operador == "+") {
+//     $resultado = $valor1 + $valor2;
+// }
+// else if ($operador == "-") {
+//     $resultado = $valor1 - $valor2;
+// }
+// else if ($operador == "*") {
+//     $resultado = $valor1 * $valor2;
+// }
+// else if ($operador == "/") {
+//     $resultado = $valor1 / $valor2;
+// }
+// else {
+//     echo "Operador inválido.";
+//     $resultado = 0;
+// }
 
-echo "O resultado da operação($valor1 $operador $valor2) é: $resultado";
-echo "<br><br>";
-
-
-
-// Ler 3 valores(A, B e C) e informar se eles sao iguais.
-$valorA = 10;
-$valorB = 10;
-$valorC = 10;
-
-if ($valorA == $valorB && $valorA == $valorC) {
-    echo "Os 3 valores sao iguais.";
-} else {
-    echo "Os valores sao diferentes";
-}
+// echo "O resultado da operação($valor1 $operador $valor2) é: $resultado";
+// echo "<br><br>";
 
 
-echo "<br><br>";
-/* Ler 3 valores(A, B e C) e informar ao usuario qual tipo de triangulo ele forma.
-    OU se não formam nenhum triangulo. 
-    Triangulos a considerar: equilatero, isoceles e escaleno 
-*/
-$valorA = 7;
-$valorB = 8;
-$valorC = 8;
+
+// // Ler 3 valores(A, B e C) e informar se eles sao iguais.
+// $valorA = 10;
+// $valorB = 10;
+// $valorC = 10;
+
+// if ($valorA == $valorB && $valorA == $valorC) {
+//     echo "Os 3 valores sao iguais.";
+// } else {
+//     echo "Os valores sao diferentes";
+// }
 
 
-if ($valorA == $valorB && $valorA == $valorC) {
-    echo "Os valores informados formam um triangulo equilatero.";
-} 
-else if (
-    ($valorA == $valorB && $valorA > $valorC) ||
-    ($valorB == $valorC && $valorB > $valorA) ||
-    ($valorA == $valorC && $valorA > $valorB)
-    ) {
-    echo "Os valores informados formam um triangulo isoceles.";
-}
-else if (
-    ($valorA != $valorB && $valorB != $valorC) &&
-    ($valorA + $valorB > $valorC)
-    ) {
-         echo "Os valores informados formam um triangulo escaleno.";
-    }
-else {
-    echo "Os valores informados NÃO formam um triangulo";
-}
+// echo "<br><br>";
+// /* Ler 3 valores(A, B e C) e informar ao usuario qual tipo de triangulo ele forma.
+//     OU se não formam nenhum triangulo. 
+//     Triangulos a considerar: equilatero, isoceles e escaleno 
+// */
+// $valorA = 7;
+// $valorB = 8;
+// $valorC = 8;
 
 
-echo "<br><br>";
+// if ($valorA == $valorB && $valorA == $valorC) {
+//     echo "Os valores informados formam um triangulo equilatero.";
+// } 
+// else if (
+//     ($valorA == $valorB && $valorA > $valorC) ||
+//     ($valorB == $valorC && $valorB > $valorA) ||
+//     ($valorA == $valorC && $valorA > $valorB)
+//     ) {
+//     echo "Os valores informados formam um triangulo isoceles.";
+// }
+// else if (
+//     ($valorA != $valorB && $valorB != $valorC) &&
+//     ($valorA + $valorB > $valorC)
+//     ) {
+//          echo "Os valores informados formam um triangulo escaleno.";
+//     }
+// else {
+//     echo "Os valores informados NÃO formam um triangulo";
+// }
+
+
+// echo "<br><br>";
 
 
 $multiplicador = 5;
@@ -139,4 +139,63 @@ for ($i = 0; $i <= 10; $i++) {
     echo "$multiplicador * $i = $resultado" . "<br>";
 }
 
+echo "<br><br>";
 
+// $i = 0;
+// while ($i <= 10) {
+    
+//     $resultado = $multiplicador * $i;
+//     echo "$multiplicador * $i = $resultado" . "<br>";
+    
+//     $i++;
+// }
+
+
+//  + - * / %(modulo)
+
+$resultado = 10 / 2 ; // 5 Quociente
+$resultado = 10 % 2 ; // 0 resto da divisao
+
+$numero = 10;
+$resto = $numero % 2 ; // 1
+$par = $resto == 0; // verdade true
+
+
+
+
+// Encontrar os 20 primeiros numeros pares e exibi-los
+
+$contPares = 0;
+for ($numero = 2; $contPares < 20; $numero++) {
+    $resto = $numero % 2;
+    $par = $resto == 0;
+
+    if ($par) {
+        echo "O numero $numero é par! <br>";
+        $contPares++;
+    }
+}
+
+
+
+// Criar uma contagem regressiva de 10 - 0
+
+// Encontrar os 5 primeiros numeros primos;
+// 2, 3, 5, 7, 11, 13, 17, 19,
+$contPrimos = 0;
+
+for ($numeroAvaliado = 3; $contPrimos < 5; $numeroAvaliado++) {
+    $penultimoNumero = $numeroAvaliado - 1;
+
+    for($divisor = 2; $divisor <= $penultimoNumero; $divisor++) {
+
+        $resto = $numero % 2;
+        $divisaoExata = $resto == 0;
+
+        if ($divisaoExata && $divisor < $numeroAvaliado -1) {
+            continue;
+        }
+
+        // implementar a logica
+    }
+}
