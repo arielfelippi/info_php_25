6491c83dfdf918feffb164ffa3738e976ea10dcd
suporte <suporte@BG-LABX-PCX>
2025-06-24 21:02:41 -0300

    exercicio procurar palavra a frase

--- index.php
+++ index.php
@@ -136,7 +136,7 @@ $multiplicador = 5;
 for ($i = 0; $i <= 10; $i++) {
     $resultado = $multiplicador * $i;
 
-    echo "$multiplicador * $i = $resultado" . "<br>";
+    // echo "$multiplicador * $i = $resultado" . "<br>";
 }
 
 echo "<br><br>";
@@ -171,7 +171,7 @@ for ($numero = 2; $contPares < 20; $numero++) {
     $par = $resto == 0;
 
     if ($par) {
-        echo "O numero $numero é par! <br>";
+        // echo "O numero $numero é par! <br>";
         $contPares++;
     }
 }
@@ -239,7 +239,7 @@ secretPass
 for ($i = 0; $i < count($primos); $i++) {
     $primo = $primos[$i];
 
-    echo "Primo: $primo" . "<br>";
+    // echo "Primo: $primo" . "<br>";
 }
 
 
@@ -262,23 +262,65 @@ for ($i = $tamanho; $i >= 0; $i--) {
     $a++;
 }
 
-if ($ehPalindromo ) {
-    echo "A palavra $palavra é um palíndromo.";
-}
-else {
-  echo "A palavra $palavra NÃO é palíndromo.";
-}
+// if ($ehPalindromo ) {
+//     echo "A palavra $palavra é um palíndromo.";
+// }
+// else {
+//   echo "A palavra $palavra NÃO é palíndromo.";
+// }
 
 /*
  Procurar uma palavra no meio da frase e informar se encontrou a mesma.
+
+ $i
+ E s c o l a     i n f o s e r v        c u r s o   d e   P H P .
+ 0 1 2 3 4 5  6  7 8 9 10 11 12 13  14  15 16 17 18  19  20 21  22 23 24 25 26 27 28
+
+$j
+ c u r s o
+ 0 1 2 3 4
+
 */
 
 $palavraProcurada = "curso";
-$frase = "Escola infoserv de PHP urso."; // curso
+$frase = "Escola infoserv curso de PHP."; // curso
 
-$tamanho = strlen($frase) - 1;
+$tamanho = strlen($frase) - 1; // 28
+$tamanhoPalavra = strlen($palavraProcurada) - 1; // 28
 
-for ($i = 0; $i <= $tamanho; $i++) {
+$tamanhoReal = $tamanho - $tamanhoPalavra;
 
+$palavraEncontrada = false;
+
+// percorre a frase toda, letra a letra
+for ($i = 0; $i <= $tamanhoReal; $i++) {
+
+    $letrasIguais = true;
+    
+    // percorre toda a palavra, letra a letra
+    for ($j = 0; $j <= $tamanhoPalavra; $j++) {
+        
+        $indiceAtualDaFrase = $i + $j;
+        // $k = $i + $j;
+
+        $letraAtualDaFrase = $frase[$indiceAtualDaFrase];
+        $letraAtualDaPalavra = $palavraProcurada[$j];
+
+        if ($letraAtualDaFrase != $letraAtualDaPalavra) {
+             $letrasIguais = false;
+            break;
+        } 
+
+    }
+
+    if ($letrasIguais) {
+        $palavraEncontrada = true;
+        break;
+    } 
 }
 
+if ($palavraEncontrada) {
+    echo "Encontrou a palavra procurada.";
+} else {
+    echo "Não Encontrou.";
+}
