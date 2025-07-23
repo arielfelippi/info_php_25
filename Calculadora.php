13fcce13ca3e8449d1f0007455978d2660f8e205
suporte <suporte@BG-LABX-PCX.localdomain>
2025-07-22 21:50:22 -0300

    calculadora completa

--- Calculadora.php
+++ Calculadora.php
@@ -9,11 +9,26 @@ class Calculadora {
         $this->resultado = $valor1 + $valor2;
     }
 
+    public function subtrair($valor1, $valor2) {
+        $this->resultado = $valor1 - $valor2;
+    }
+
+    public function multiplicar($valor1, $valor2) {
+        $this->resultado = $valor1 * $valor2;
+    }
+
+    public function dividir($valor1, $valor2) {
+        $this->resultado = $valor1 / $valor2;
+    }
+
     public function exibir() {
-        echo $this->resultado;
+        echo $this->resultado . "<br>";
     }
 }
 
 $objCalculadora = new Calculadora();
 $objCalculadora->somar(10, 10);
+$objCalculadora->exibir();
+
+$objCalculadora->multiplicar(10, 10);
 $objCalculadora->exibir();
