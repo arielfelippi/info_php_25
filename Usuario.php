90c4e7734bd84f991b972eac717498b6e92c550d
root <root@BG-LABX-PCX.localdomain>
2025-08-05 19:51:49 -0300

    objetos para o banco de dados

---
+++ Usuario.php
@@ -0,0 +1,11 @@
+<?php
+
+class Usuario {
+    public $login = "";
+    public $senha = "";
+    public $nomeUsuario = "";
+    public $emailRecuperacao = "";
+    public $status = "";
+    public $tipoPerfil = "";
+    public $permissoes = "";
+}
